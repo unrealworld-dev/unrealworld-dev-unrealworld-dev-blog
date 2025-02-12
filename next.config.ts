1014b4ac8d5eeaed71164cd6b143cdf94ebc4ce1
unrealworld-dev <unrealworld.dev@gmail.com>
2025-02-12 11:21:06 +0900

    mdx setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
+import { build } from 'velite'
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const isDev = process.argv.indexOf('dev') !== -1
+const isBuild = process.argv.indexOf('build') !== -1
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = '1'
+  import('velite').then(v => v.build({ watch: isDev, clean: !isDev }))
+}
+
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler: { hooks: { beforeCompile: { tapPromise: (arg0: string, arg1: () => Promise<void>) => void; }; }; options: { mode: string; }; }) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      await build({ watch: dev, clean: !dev })
+    })
+  }
+}
+
+/** @type {import('next').NextConfig} */
+export default {
+  // next config here...
+  webpack: (config: { plugins: VeliteWebpackPlugin[]; }) => {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
+  }
+}
+
---
+++ velite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, defineCollection, s } from 'velite'
+
+// `s` is extended from Zod with some custom schemas,
+// you can also import re-exported `z` from `velite` if you don't need these extension schemas.
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+    ...data,
+    slugAsParams: data.slug.split("/").slice(1).join("/"),
+})
+
+const posts = defineCollection({
+    name: "Post",
+    pattern: "blog/**/*.mdx",
+    schema: s.object({
+        slug: s.path(),
+        title: s.string().max(99),
+        description: s.string().max(999).optional(),
+        date: s.isodate(),
+        cover: s.image(),
+        published: s.boolean().default(true),
+        content: s.mdx()
+        // 이미지 추가필요
+    })
+        .transform(computedFields),
+})
+
+export default defineConfig({
+    root: "content",
+    output: {
+        data: ".velite",
+        assets: "public/static",
+        base: "/static/",
+        name: "[name]-[hash:6].[ext]",
+        clean: true,
+    },
+    collections: { posts },
+    mdx: {
+        rehypePlugins: [],
+        remarkPlugins: [],
+    },
+})
