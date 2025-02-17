3f0a5ecc9f5febde2b8f84f7ebdb0a9c4fff24e5
unrealworld-dev <unrealworld.dev@gmail.com>
2025-02-17 20:31:24 +0900

    nextjs mdx build completed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypeslug from "rehype-slug"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeslug,
+      [rehypePrettyCode, { theme: "github-dark" }],
+      [rehypeAutolinkHeadings, {
+        behavior: "wrap",
+        properties: {
+          className: ["subheading-anchor"],
+          ariaLabel: "Link to section",
+        },
+      }],
+    ],
+  },
+})
+
+export default withMDX(nextConfig);
